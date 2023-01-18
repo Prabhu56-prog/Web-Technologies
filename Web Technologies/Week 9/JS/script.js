e9d19b2fc8bcc8df69c1297ff60776a9f2545949
Prabhu Pradhan <119127560+pradhanprabhu@users.noreply.github.com>
2023-01-18 13:42:07 +0545

    created week 9 folder

---
+++ Web Technologies/Week 9/JS/script.js
@@ -0,0 +1,18 @@
+ alert("Don't touch my pc");
+console.log("hello from the alien");
+console.log(document.getElementById("paragraph").innerHTML)
+ document.getElementById("paragraph").innerHTML="<h1>hello world</h1>";
+/* document.getElementById("paragraph").style.color="blue"; 
+document.getElementById("paragraph").style.backgroundColor="green"; */
+document.getElementById("paragraph").style.cssText="color:purple;backgroundColor:aqua";
+var age=10;
+age=12;
+const grade=12;
+//grade=13;
+let age3=19;
+age3=20;
+var name="Prabhu";
+console.log(age);
+console.log(grade);
+console.log(age3);
+console.log(typeof name);
